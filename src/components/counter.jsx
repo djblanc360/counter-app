ab12ea030c4c64d4a4e1bc34e64c89b026294c85
djblanc360 <djblanc360@gmail.com>
2019-03-27 22:12:25 -0700

    single counter

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
+  state = {
+    count: 0
+  };
+
   render() {
-    return ()
+    return (
+      <div>
+        <span className="badge badge-primary m-2">{this.formatCount()}</span>
+        <button type="button" className="btn btn-secondary btn-sm">Increment</button>
+      </div>
+    );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? 'Zero' : count;
   }
 }
+
+
 
 export default Counter;
