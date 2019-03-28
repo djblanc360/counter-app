4ef71870359b7e7f047aa0f370e700497b5fd52f
djblanc360 <djblanc360@gmail.com>
2019-03-27 22:22:32 -0700

    single counter

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,10 +5,14 @@ class Counter extends Component {
     count: 0
   };
 
+
   render() {
+    let classes = "badge m-2 ";
+    classes += this.state.count === 0 ? "badge-warning" : "badge-primary";
+
     return (
       <div>
-        <span className="badge badge-primary m-2">{this.formatCount()}</span>
+        <span className={classes}>{this.formatCount()}</span>
         <button type="button" className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
